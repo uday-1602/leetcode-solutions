391dcdfc4ffcdd9cde463324e9c8254c02acec2c
uday-1602 <155349309+uday-1602@users.noreply.github.com>
2026-03-06 22:27:43 +0530

    Solved: Sort Colors (ID: 75)

---
+++ 0075-sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i = 0; i<nums.size()-1; i++){
+            int minIndex = i;
+            for(int j = i+1; j<nums.size(); j++){
+                if(nums[j]<nums[minIndex]){
+                    minIndex = j;
+                }
+            }
+            swap(nums[minIndex],nums[i]);
+        }
+    }
+};
